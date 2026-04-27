055e32067946b91627528bd64a22cb0a1cdf530f
fly <hntaxx@gmail.com>
2026-04-27 16:24:16 +0900

    mainView.jsのgetRecordsの変数を統一

--- js/mainView.js
+++ js/mainView.js
@@ -1,29 +1,7 @@
-import { getRecordsByUid } from "./db.js";
-import { auth } from "./firebase.js";
-
-// 活動内容のマスタ
-const DOING_OPTIONS = {
-  1: "インターン",
-  2: "インターン選考",
-  3: "オープンカンパニー",
-  4: "個別説明会",
-  5: "合同説明会",
-  6: "選考or受験",
-  7: "内定",
-  8: "内定後の集まり",
-  9: "その他",
-};
-
-// 試験内容のマスタ
-const TEST_OPTIONS = {
-  1: "筆記試験",
-  2: "面接試験",
-  3: "グループワーク",
-  4: "その他",
-};
+import { getRecords } from "./db.js";
 
 export const loadRecords = async () => {
-  const records = await getRecordsByUid("records", auth.currentUser.uid);
+  const records = await getRecords("records");
   const tbody = document.getElementById("infoTableBody");
   tbody.innerHTML = "";
 
